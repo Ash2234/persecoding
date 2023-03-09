ab6d3932229e4788a29ccc8f031d377edf4017f5
Ash2234 <125533917+Ash2234@users.noreply.github.com>
2023-03-08 21:04:28 -0800

    Update and rename Task6 to Task6.py

---
+++ specimenp1/Task6.py
@@ -0,0 +1,15 @@
+first=input()
+second=input()
+third=input()
+fourth=input()
+fifth=input()
+if first=='Ellie':
+    print('1st')
+elif second=='Ellie':
+    print('2nd')
+elif third=='Ellie':
+    print('3rd')
+elif fourth=='Ellie':
+    print('4th')
+else:
+    print('5th')
