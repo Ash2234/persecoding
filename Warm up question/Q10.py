abf934074272755695559f87bdd5bf28d0f85a14
Ash2234 <125533917+Ash2234@users.noreply.github.com>
2023-03-22 22:07:53 +0800

    Create Q10.py

---
+++ Warm up question/Q10.py
@@ -0,0 +1,18 @@
+x=int(input())
+y=-(int(input()))
+n=int(input())
+old_deck=[1,2,3,4,5,6,7,8,9]
+new_deck=[]
+x2=0
+y2=-1
+n2=0
+while n2<=n or old_deck!=[]:
+  while x2<=x:
+    new_deck.append(old_deck[x2])
+    x2+=1
+  x2=0
+  while y2<=y:
+    new_deck.append(old_deck[y2])
+    y2-=1
+  y2=-1
+print(new_deck[1])
